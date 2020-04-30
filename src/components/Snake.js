af958462e1664d35a5eb1852882dbe640473d95e
Shokupanman <Harrisonkaidehancock@gmail.com>
2020-04-30 09:27:06 +0900

    working on grid still, implicating a effect hook

--- src/components/Snake.js
+++ src/components/Snake.js
@@ -1,9 +1,37 @@
 import React, { useState } from 'react'
 import './Snake.scss'
 
-function Snake() {
+const Snake = () => {
   let [grid, setGrid] = useState([])
   let [rows, setRows] = useState(10)
   let [cols, setCols] = useState(10)
+
+  // const updateGrid = (k,v) => {
+  //   setGrid(grid.set(k,v))
+  // }
+  const snakeGrid = []
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      snakeGrid.push({
+        row,
+        col,
+      })
+    }
+  }
+  setGrid({ grid: snakeGrid })
+
+  const gridItems = grid.map((snakeGrid) => {
+    return (
+      <div
+        key={snakeGrid.row.toString() + '-' + snakeGrid.col.toString()}
+        className="grid-item"
+      ></div>
+    )
+  })
+  return (
+    <div className="snake-container">
+      <section className="grid">{gridItems}</section>
+    </div>
+  )
 }
 export default Snake
