def6726bb68d2ee86d793d031439f32f32969619
Shokupanman <Harrisonkaidehancock@gmail.com>
2020-05-05 15:43:16 +0900

    changed method and found new resource, made snake body and grid demo

--- src/components/Snake.js
+++ src/components/Snake.js
@@ -2,35 +2,11 @@ import React, { useState } from 'react'
 import './Snake.scss'
 
 const Snake = () => {
-  let [grid, setGrid] = useState([])
-  let [rows, setRows] = useState(10)
-  let [cols, setCols] = useState(10)
-
-  // const updateGrid = (k,v) => {
-  //   setGrid(grid.set(k,v))
-  // }
-  const snakeGrid = []
-  for (let row = 0; row < rows; row++) {
-    for (let col = 0; col < cols; col++) {
-      snakeGrid.push({
-        row,
-        col,
-      })
-    }
-  }
-  setGrid({ grid: snakeGrid })
-
-  const gridItems = grid.map((snakeGrid) => {
-    return (
-      <div
-        key={snakeGrid.row.toString() + '-' + snakeGrid.col.toString()}
-        className="grid-item"
-      ></div>
-    )
-  })
   return (
-    <div className="snake-container">
-      <section className="grid">{gridItems}</section>
+    <div className="snakeArena">
+      <div className="snake" style={{ top: 0, left: 0 }}></div>
+      <div className="snake" style={{ top: 0, left: '2%' }}></div>
+      <div className="snake" style={{ top: 0, left: '4% ' }}></div>
     </div>
   )
 }
